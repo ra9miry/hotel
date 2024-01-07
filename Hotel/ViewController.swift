6e6067cec0df168f3ba5817d7fe3486c4339569c
Radmiry <telmanradmir@gmail.com>
2024-01-07 19:15:11 +0600

    Initial Commit

---
+++ Hotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel
+//
+//  Created by Радмир Тельман on 07.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
